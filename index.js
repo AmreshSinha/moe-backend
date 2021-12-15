3022d9f9bb4bdb368e15562a1c9d163daa12bb6a
AmreshSinha <theamresh.sinha@gmail.com>
2021-12-15 14:08:47 +0530

    Change Some Text for Console 💻

--- index.js
+++ index.js
@@ -44,5 +44,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`API listening at http://localhost:${port}`);
 });
